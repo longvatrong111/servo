eb602413f095b31f8f26eb50816decf605dc611f
batu_hoang <longvatrong111@gmail.com>
2025-06-13 17:22:11 +0800

    Implement webdriver release action

--- components/webdriver_server/lib.rs
+++ components/webdriver_server/lib.rs
@@ -143,11 +143,19 @@ pub fn start_server(port: u16, constellation_chan: Sender<EmbedderToConstellatio
 }
 
 /// Represents the current WebDriver session and holds relevant session state.
+/// Currently, only 1 webview is supported per session.
+/// So only there is only 1 InputState.
 pub struct WebDriverSession {
+    /// <https://www.w3.org/TR/webdriver2/#dfn-session-id>
     id: Uuid,
-    browsing_context_id: BrowsingContextId,
+    
+    /// <https://www.w3.org/TR/webdriver2/#dfn-current-top-level-browsing-context>
     webview_id: WebViewId,
 
+    /// <https://www.w3.org/TR/webdriver2/#dfn-current-browsing-context>
+    browsing_context_id: BrowsingContextId,
+
+    /// <https://www.w3.org/TR/webdriver2/#dfn-window-handles>
     window_handles: HashMap<WebViewId, String>,
 
     /// Time to wait for injected scripts to run before interrupting them.  A [`None`] value
@@ -201,6 +209,8 @@ impl WebDriverSession {
     }
 }
 
+/// The `Handler` for the WebDriver server.
+/// Currently, only 1 session is supported.
 struct Handler {
     /// The threaded receiver on which we can block for a load-status.
     /// It will receive messages sent on the load_status_sender,
@@ -1583,12 +1593,15 @@ impl Handler {
 
     /// <https://w3c.github.io/webdriver/#dfn-release-actions>
     fn handle_release_actions(&mut self) -> WebDriverResult<WebDriverResponse> {
-        // TODO: The previous implementation of this function was different from the spec.
-        // Need to re-implement this to match the spec.
         let session = self.session()?;
+        
         // Step 1. If session's current browsing context is no longer open,
         // return error with error code no such window.
         self.verify_browsing_context_is_open(session.browsing_context_id)?;
+
+        // TODO: Step 2. User prompts. No user prompt implemented yet.
+
+        // Step 8. Reset the input state of session's current top-level browsing context.
         session.input_state_table.borrow_mut().clear();
 
         Ok(WebDriverResponse::Void)
